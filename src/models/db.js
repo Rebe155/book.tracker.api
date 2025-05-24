a9ae9ed1fd41472322f70deb603039af6e059c98
Rebeca Arévalo <arevalorebeca41@gmail.com>
2025-05-23 23:49:18 -0600

    routes

--- src/models/db.js
+++ src/models/db.js
@@ -1,20 +1,18 @@
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config");
 
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  port: dbConfig.PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-connection.connect(error => {
-  if (error) {
-    console.error("Error al conectar a la base de datos:", error.message);
-    process.exit(1); 
+connection.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a la base de datos:", err);
+    return;
   }
-  console.log("Conectado correctamente a la base de datos MySQL en Railway.");
+  console.log("Conectado a la base de datos MySQL");
 });
 
 module.exports = connection;
